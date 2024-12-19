aa513675f52f8c24cf555d2467b5d9a4dadd992b
laFemboy <109281@g.elearn.uz.zgora.pl>
2024-12-19 23:26:36 +0100

    Menu Główne skończone

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,25 +3,72 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public GameObject panel;
+    public GameObject panelOGrze;
+    public GameObject panelWyjscieZGry;
 
     void Start()
     {
-        panel.SetActive(false);
+        if (SprawdzeniePaneliCzyNieNull())
+            return;
+
+        TogglePanels();
+    }
+
+    void Update()
+    {
+        IfEscPressed();
     }
 
     public void Wroc()
     {
-        panel.SetActive(false);
+        panelOGrze.SetActive(false);
+        panelWyjscieZGry.SetActive(false);
     }
 
-    public void Sterowanie()
+    public void OGrze()
     {
-        panel.SetActive(true);
+        panelOGrze.SetActive(true);
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene("Hangar");
+    }
+
+    private bool SprawdzeniePaneliCzyNieNull(){
+        if (panelOGrze == null || panelWyjscieZGry == null)
+        {
+            Debug.LogError("Jedno z wymaganych pól nie jest przypisane.");
+            return true;
+        }
+        return false;
+    }
+
+    public void WyjscieZGry()
+    {
+        Application.Quit();
+    }
+
+    private void TogglePanels()
+    {
+        panelOGrze.SetActive(false);
+        panelWyjscieZGry.SetActive(false);
+    }
+
+    private void IfEscPressed()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (panelOGrze.activeSelf)
+            {
+                Wroc();
+                return;
+            }
+            
+            if(panelWyjscieZGry.activeSelf)
+                panelWyjscieZGry.SetActive(false);
+            else
+                panelWyjscieZGry.SetActive(true);
+        }
     }
 }
