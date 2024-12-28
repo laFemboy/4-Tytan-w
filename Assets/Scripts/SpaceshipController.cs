298736d5dfb8ba831d2f717cab84cf719c9c9792
ShHryhorii <163681108+ShHryhorii@users.noreply.github.com>
2024-12-28 23:32:40 +0100

    Update SpaceshipController.cs

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -4,7 +4,7 @@ public class SpaceshipController : MonoBehaviour
 {
     public float moveSpeed = 30f;          
     public float sprintMultiplier = 2f;   
-    public float rotationSpeed = 50f;    
+    public float rotationSpeed = 50f;
 
     void Update()
     {
@@ -16,9 +16,9 @@ public class SpaceshipController : MonoBehaviour
         float moveLeftRight = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
         float moveUpDown = 0;
 
-        if (Input.GetKey(KeyCode.Q))   
+        if (Input.GetKey(KeyCode.Q))
             moveUpDown = moveSpeed * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.E))   
+        else if (Input.GetKey(KeyCode.E))
             moveUpDown = -moveSpeed * Time.deltaTime;
 
         Vector3 movement = transform.forward * moveForwardBackward +
@@ -26,13 +26,13 @@ public class SpaceshipController : MonoBehaviour
                            transform.up * moveUpDown;
         transform.position += movement;
 
-        float pitch = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime; 
-        float yaw = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime; 
+        float pitch = Input.GetAxis("Mouse Y") * rotationSpeed * Time.deltaTime;
+        float yaw = Input.GetAxis("Mouse X") * rotationSpeed * Time.deltaTime;
         float roll = 0;
 
-        if (Input.GetKey(KeyCode.A))  
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))  
             roll = rotationSpeed * Time.deltaTime;
-        else if (Input.GetKey(KeyCode.D))  
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))  
             roll = -rotationSpeed * Time.deltaTime;
 
         transform.Rotate(pitch, yaw, roll, Space.Self);
