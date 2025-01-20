850e8526a233747a79a3790e97e04d1a92f2530b
Cinu13 <marcinturkowskins@gmail.com>
2025-01-20 17:04:23 +0100

    Ulepszone Planety
    Teraz sie krecą :)

--- Assets/Scripts/SpaceshipController.cs
+++ Assets/Scripts/SpaceshipController.cs
@@ -8,12 +8,6 @@ public class SpaceshipController : MonoBehaviour
     public float rollAcceleration;
     public float rollDamping;
 
-    public GameObject laserPrefab; // Prefab lasera
-    public Transform laserSpawnPoint; // Punkt, z którego wystrzeliwany jest laser
-    public float laserSpeed = 20f; // Prędkość lasera
-    public float laserLifeTime = 2f; // Czas życia lasera
-
-    public LayerMask enemyLayer; // Warstwa, na której znajdują się przeciwnicy
 
     private float currentSpeed;
     private float currentRollVelocity;
@@ -62,49 +56,7 @@ public class SpaceshipController : MonoBehaviour
         currentRollVelocity = Mathf.Clamp(currentRollVelocity, -rotationSpeed, rotationSpeed);
         transform.Rotate(pitch, yaw, currentRollVelocity * Time.deltaTime, Space.Self);
 
-        // Strzelanie laserami
-        if (Input.GetMouseButtonDown(0)) // Lewy przycisk myszy
-        {
-            ShootLaser();
-        }
     }
 
-    void ShootLaser()
-    {
-        // Sprawdzenie, czy prefab lasera i punkt strzału są przypisane
-        if (laserPrefab != null && laserSpawnPoint != null)
-        {
-            // Tworzymy laser w punkcie startowym
-            GameObject laser = Instantiate(laserPrefab, laserSpawnPoint.position, laserSpawnPoint.rotation);
 
-            // Pobieramy Rigidbody lasera, aby nadawać mu prędkość
-            Rigidbody laserRb = laser.GetComponent<Rigidbody>();
-
-            if (laserRb != null)
-            {
-                // Szukamy najbliższego przeciwnika
-                Collider[] enemies = Physics.OverlapSphere(transform.position, 100f, enemyLayer);
-                Transform target = null;
-                if (enemies.Length > 0)
-                {
-                    // Przeciwnik to pierwszy znaleziony obiekt
-                    target = enemies[0].transform;
-
-                    // Obliczamy kierunek do przeciwnika
-                    Vector3 directionToTarget = (target.position - laser.transform.position).normalized;
-
-                    // Nadajemy prędkość w kierunku przeciwnika
-                    laserRb.linearVelocity = directionToTarget * laserSpeed;
-                }
-                else
-                {
-                    // Jeśli nie ma przeciwnika, laser leci w normalnym kierunku
-                    laserRb.linearVelocity = transform.forward * laserSpeed;
-                }
-            }
-
-            // Usuwamy laser po określonym czasie życia
-            Destroy(laser, laserLifeTime);
-        }
-    }
 }
